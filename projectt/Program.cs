202d108e0f7347c8f637b22ce756658afde348ed
Martin Kronborg <martinkronborg@hotmail.dk>
2024-10-11 17:51:09 +0200

    Arv Lagersystem delopgave 1 og 2

--- projectt/Program.cs
+++ projectt/Program.cs
@@ -2,7 +2,23 @@
 
 FoodItem to = new FoodItem("hvidløgsflutes",5,79);
 
+NonFoodItem tre = new NonFoodItem("atombombe",7500000,["uran", "plutonium", "steel", "din mor"]);
+
 //Console.WriteLine(et.GetName());
 //Console.WriteLine(et.GetPrice());
 
-Console.WriteLine(to.ToString());
+//Console.WriteLine(to.ToString());
+
+//Console.WriteLine(tre.ToString());
+
+
+FoodItem[] Items = new FoodItem[10];
+for (int i = 0; i < Items.Length; i++)
+{
+	Items[i] = new FoodItem($"Item {i}",12.3 * i, DateTime.Now.Day);
+}
+
+foreach (FoodItem Item in Items)
+{
+	Console.WriteLine(Item.ToString());
+}
