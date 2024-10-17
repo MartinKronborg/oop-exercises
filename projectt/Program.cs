69645ec184353c53601256414811a74f0be9cb99
Martin Kronborg <martinkronborg@hotmail.dk>
2024-10-17 17:27:18 +0200

    Standardbibliotek tidstagning delopgave 1

--- projectt/Program.cs
+++ projectt/Program.cs
@@ -1,5 +1,32 @@
-﻿for (int i = 0; i < 9002; i++)
-{
-	new Kitten(i);
+﻿using System;
+
+var initialValue = 1.0000001;
+
+for (double i = 1; i <= 32; i++)
+	{
+		long t0 = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+
+		double f = Timing.Fun(initialValue,i);
+		long t1 = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+
+		Console.WriteLine("fun(" + initialValue + "," + i + ") = " + f + " (calculation took " + (t1-t0) + " ms)");
+		
+
+
+	}	
+
+public static class Timing
+{	
+
+	public static double Fun(double x, double y)
+	{
+		if (y <= 1)
+		{
+			return x;
+		}
+		else 
+		{
+			return Fun(x,y-1) * Fun(x,y-1);
+		}
+	}
 }
-Console.WriteLine("There are " + Kitten.count + " kittens.");
